e157a48c391f0eaebda49f43557946d1575fc4b3
Anderson <andersonribeiro.si@hotmail.com>
2020-04-26 17:50:35 -0300

    Api de clientes para ordens de serviços - CRUD

---
+++ src/main/java/com/osworks/api/domain/exception/RegraNegocioException.java
@@ -0,0 +1,11 @@
+package com.osworks.api.domain.exception;
+
+public class RegraNegocioException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public RegraNegocioException(String mensagem) {
+		super(mensagem);
+	}
+
+}
